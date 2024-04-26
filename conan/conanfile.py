25a435589cf98ebf61cb6ffec6a4f6127f0ced17
Mykola Solianko <mykola_solianko@epam.com>
2024-04-26 13:41:43 +0300

    Add conanfile.py and host_build.sh scripts
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ conan/conanfile.py
@@ -0,0 +1,13 @@
+from conan import ConanFile
+
+class AosCommonCpp(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("gtest/1.14.0")
+        self.requires("poco/1.13.2")
+        self.requires("grpc/1.54.3")
+
+    def build_requirements(self):
+        self.tool_requires("grpc/1.54.3")
