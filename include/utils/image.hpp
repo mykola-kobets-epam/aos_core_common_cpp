b7a75aa726bf77c6ef89e143fe23426927d8aa80
Mykola Solianko <mykola_solianko@epam.com>
2024-06-17 12:25:57 +0300

    [image] Add functionality for image
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ include/utils/image.hpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef UTILS_IMAGE_HPP
+#define UTILS_IMAGE_HPP
+
+#include <string>
+
+#include <aos/common/tools/error.hpp>
+
+namespace aos::common::utils {
+
+using Digest = std::string;
+
+/**
+ * Unpacks an image archive.
+ *
+ * @param archivePath path to the archive.
+ * @param destination path to the destination directory.
+ * @return aos::Error.
+ */
+Error UnpackTarImage(const std::string& archivePath, const std::string& destination);
+
+/**
+ * Parses the digest string.
+ *
+ * @param digest digest string.
+ * @return std::pair<std::string, std::string> .
+ */
+std::pair<std::string, std::string> ParseDigest(const std::string& digest);
+
+/**
+ * Validates the digest.
+ *
+ * @param digest digest string.
+ */
+Error ValidateDigest(const Digest& digest);
+
+/**
+ * Hashes the directory.
+ *
+ * @param dir directory path.
+ * @return std::string.
+ */
+RetWithError<std::string> HashDir(const std::string& dir);
+
+} // namespace aos::common::utils
+
+#endif // UTILS_IMAGE_HPP
