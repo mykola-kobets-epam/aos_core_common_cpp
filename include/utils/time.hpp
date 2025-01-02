b05ca2cc6ad8f5e87ce2eb70e67753cddd369c38
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2025-01-02 18:35:23 +0200

    [utils,time] Add time utils to work with UTC
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

--- include/utils/time.hpp
+++ include/utils/time.hpp
@@ -13,6 +13,7 @@
 #include <string>
 
 #include <aos/common/tools/error.hpp>
+#include <aos/common/tools/time.hpp>
 
 namespace aos::common::utils {
 
@@ -36,6 +37,38 @@ using Duration = std::chrono::duration<int64_t, std::nano>;
  * @return parsed duration.
  */
 aos::RetWithError<Duration> ParseDuration(const std::string& duration);
+
+/**
+ * Parses ISO8601 duration from string.
+ *
+ * @param duration duration string.
+ * @return parsed duration.
+ */
+aos::RetWithError<Duration> ParseISO8601Duration(const std::string& duration);
+
+/**
+ * Formats ISO8601 duration string.
+ *
+ * @param duration duration.
+ * @return aos::RetWithError<std::string>.
+ */
+aos::RetWithError<std::string> FormatISO8601Duration(const Duration& duration);
+
+/**
+ * Creates time object from a UTC formatted string.
+ *
+ * @param utcTimeStr UTC formatted time string.
+ * @return aos::RetWithError<aos::Time>.
+ */
+aos::RetWithError<aos::Time> FromUTCString(const std::string& utcTimeStr);
+
+/**
+ * Converts time into a UTC string.
+ *
+ * @param time time object.
+ * @return aos::RetWithError<std::string>.
+ */
+aos::RetWithError<std::string> ToUTCString(const aos::Time& time);
 
 } // namespace aos::common::utils
 
