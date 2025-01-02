d656f2c2fdcb500f66afa27338df0b84aec2a2e4
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2025-01-02 18:35:23 +0200

    [utils,time] Add time utils to work with UTC
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

--- src/utils/time.cpp
+++ src/utils/time.cpp
@@ -5,13 +5,165 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <array>
 #include <map>
 #include <regex>
 #include <sstream>
 
+#include <aos/common/tools/time.hpp>
+
 #include "utils/time.hpp"
 
 namespace aos::common::utils {
+
+namespace {
+
+/***********************************************************************************************************************
+ * Constants
+ **********************************************************************************************************************/
+
+constexpr auto cSecondDuration = Duration(std::chrono::seconds(1));
+constexpr auto cMinuteDuration = Duration(std::chrono::minutes(1));
+constexpr auto cHourDuration   = Duration(std::chrono::hours(1));
+constexpr auto cDayDuration    = cHourDuration * 24;
+constexpr auto cWeekDuration   = 7 * cDayDuration;
+constexpr auto cYearDuration   = 365 * cDayDuration;
+constexpr auto cMonthDuration  = cYearDuration / 12;
+
+/***********************************************************************************************************************
+ * Static
+ **********************************************************************************************************************/
+
+aos::RetWithError<Duration> ParseISO8601DurationPeriod(const std::string& period)
+{
+    Duration totalDuration {};
+
+    if (period.empty()) {
+        return {totalDuration};
+    }
+
+    std::smatch match;
+    std::regex  iso8601DurationPattern(R"(P(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)W)?(?:(\d+)D)?)");
+
+    if (!std::regex_match(period, match, iso8601DurationPattern) || match.size() != 5) {
+        return {{}, Error(aos::ErrorEnum::eInvalidArgument, "invalid ISO8601 duration format")};
+    }
+
+    if (match[1].matched) {
+        totalDuration += std::stoi(match[1].str()) * cYearDuration;
+    }
+
+    if (match[2].matched) {
+        totalDuration += std::stoi(match[2].str()) * cMonthDuration;
+    }
+
+    if (match[3].matched) {
+        totalDuration += std::stoi(match[3].str()) * cWeekDuration;
+    }
+
+    if (match[4].matched) {
+        totalDuration += std::stoi(match[4].str()) * cDayDuration;
+    }
+
+    return totalDuration;
+}
+
+aos::RetWithError<Duration> ParseISO8601DurationTime(const std::string& time)
+{
+    std::chrono::nanoseconds totalDuration {};
+
+    if (time.empty()) {
+        return {totalDuration};
+    }
+
+    std::smatch match;
+    std::regex  iso8601DurationPattern(R"(T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?)");
+
+    if (!std::regex_match(time, match, iso8601DurationPattern) || match.size() != 4) {
+        return {{}, Error(aos::ErrorEnum::eInvalidArgument, "invalid ISO8601 duration format")};
+    }
+
+    if (match[1].matched) {
+        totalDuration += std::stoi(match[1].str()) * cHourDuration;
+    }
+
+    if (match[2].matched) {
+        totalDuration += std::stoi(match[2].str()) * cMinuteDuration;
+    }
+
+    if (match[3].matched) {
+        totalDuration += std::stoi(match[3].str()) * cSecondDuration;
+    }
+
+    return totalDuration;
+}
+
+std::string FormatISO8601DurationPeriod(int64_t& total)
+{
+    std::ostringstream oss;
+
+    oss << "P";
+
+    if (auto years = total / cYearDuration.count(); years > 0) {
+        oss << years << "Y";
+
+        total %= cYearDuration.count();
+    }
+
+    if (auto months = total / cMonthDuration.count(); months > 0) {
+        oss << months << "M";
+
+        total %= cMonthDuration.count();
+    }
+
+    if (auto weeks = total / cWeekDuration.count(); weeks > 0) {
+        oss << weeks << "W";
+
+        total %= cWeekDuration.count();
+    }
+
+    if (auto days = total / cDayDuration.count(); days > 0) {
+        oss << days << "D";
+
+        total %= cDayDuration.count();
+    }
+
+    return oss.str();
+}
+
+std::string FormatISO8601DurationTime(int64_t& total)
+{
+    std::ostringstream oss;
+
+    auto hours = total / cHourDuration.count();
+    total %= cHourDuration.count();
+
+    auto minutes = total / cMinuteDuration.count();
+    total %= cMinuteDuration.count();
+
+    auto seconds = total / cSecondDuration.count();
+    total %= cSecondDuration.count();
+
+    if (hours || minutes || seconds) {
+        oss << "T";
+
+        if (hours) {
+            oss << hours << "H";
+        }
+
+        if (minutes) {
+            oss << minutes << "M";
+        }
+
+        if (seconds) {
+            oss << seconds << "S";
+        }
+    }
+
+    return oss.str();
+}
+
+}; // namespace
 
 aos::RetWithError<Duration> ParseDuration(const std::string& durationStr)
 {
@@ -42,6 +194,78 @@ aos::RetWithError<Duration> ParseDuration(const std::string& durationStr)
     }
 
     return totalDuration;
+}
+
+aos::RetWithError<Duration> ParseISO8601Duration(const std::string& duration)
+{
+    Duration    totalDuration {};
+    std::smatch match;
+    std::regex  iso8601DurationPattern(R"(^(P(?:\d+Y)?(?:\d+M)?(?:\d+W)?(?:\d+D)?)?(T(?:\d+H)?(?:\d+M)?(?:\d+S)?)?$)");
+
+    if (!std::regex_match(duration, match, iso8601DurationPattern) || match.size() != 3) {
+        return {{}, Error(aos::ErrorEnum::eInvalidArgument, "invalid ISO8601 duration format")};
+    }
+
+    auto [delta, err] = ParseISO8601DurationPeriod(match[1].str());
+    if (!err.IsNone()) {
+        return {{}, AOS_ERROR_WRAP(err)};
+    }
+
+    totalDuration += delta;
+
+    Tie(delta, err) = ParseISO8601DurationTime(match[2].str());
+    if (!err.IsNone()) {
+        return {{}, AOS_ERROR_WRAP(err)};
+    }
+
+    totalDuration += delta;
+
+    return totalDuration;
+}
+
+aos::RetWithError<std::string> FormatISO8601Duration(const Duration& duration)
+{
+    auto total = duration.count();
+
+    auto durationStr = FormatISO8601DurationPeriod(total);
+    if (durationStr.empty()) {
+        return {"", Error(ErrorEnum::eFailed, "failed to format ISO8601 duration")};
+    }
+
+    return durationStr.append(FormatISO8601DurationTime(total));
+}
+
+aos::RetWithError<aos::Time> FromUTCString(const std::string& utcTimeStr)
+{
+    struct tm timeInfo = {};
+
+    if (!strptime(utcTimeStr.c_str(), "%Y-%m-%dT%H:%M:%SZ", &timeInfo)) {
+        return {Time(), ErrorEnum::eInvalidArgument};
+    }
+
+    return {aos::Time::Unix(mktime(&timeInfo)), ErrorEnum::eNone};
+}
+
+aos::RetWithError<std::string> ToUTCString(const aos::Time& time)
+{
+    tm   timeInfo {};
+    auto timespec = time.UnixTime();
+
+    timespec.tv_sec = timegm(localtime_r(&timespec.tv_sec, &timeInfo));
+
+    if (!gmtime_r(&timespec.tv_sec, &timeInfo)) {
+        return {"", ErrorEnum::eFailed};
+    }
+
+    std::array<char, aos::cTimeStrLen> buffer;
+
+    auto size = strftime(buffer.begin(), buffer.size(), "%Y-%m-%dT%H:%M:%SZ", &timeInfo);
+
+    if (size == 0) {
+        return {"", ErrorEnum::eFailed};
+    }
+
+    return {{buffer.begin(), buffer.begin() + size}, ErrorEnum::eNone};
 }
 
 } // namespace aos::common::utils
--- tests/utils/time_test.cpp
+++ tests/utils/time_test.cpp
@@ -7,17 +7,121 @@
 
 #include <gtest/gtest.h>
 
+#include <aos/test/log.hpp>
+
+#include "utils/exception.hpp"
 #include "utils/time.hpp"
 
 using namespace testing;
 
 /***********************************************************************************************************************
+ * Static
+ **********************************************************************************************************************/
+
+namespace {
+
+void SetTimezone(const std::string& timezone)
+{
+    setenv("TZ", timezone.c_str(), 1);
+    tzset();
+}
+
+class ExpectedDateTime {
+public:
+    ExpectedDateTime& SetYear(int year)
+    {
+        mYear = year;
+        return *this;
+    }
+
+    ExpectedDateTime& SetMonth(int month)
+    {
+        mMonth = month;
+        return *this;
+    }
+
+    ExpectedDateTime& SetDay(int day)
+    {
+        mDay = day;
+        return *this;
+    }
+
+    ExpectedDateTime& SetHour(int hour)
+    {
+        mHour = hour;
+        return *this;
+    }
+
+    ExpectedDateTime& SetMinute(int minute)
+    {
+        mMinute = minute;
+        return *this;
+    }
+
+    ExpectedDateTime& SetSecond(int second)
+    {
+        mSecond = second;
+        return *this;
+    }
+
+    bool operator==(const aos::Time& time) const
+    {
+        int day, month, year, hour, minute, second;
+
+        auto err = time.GetDate(&day, &month, &year);
+        AOS_ERROR_CHECK_AND_THROW("failed to get date", err);
+
+        if (mYear.has_value() && mYear.value() != year) {
+            return false;
+        }
+
+        if (mMonth.has_value() && mMonth.value() != month) {
+            return false;
+        }
+
+        if (mDay.has_value() && mDay.value() != day) {
+            return false;
+        }
+
+        err = time.GetTime(&hour, &minute, &second);
+        AOS_ERROR_CHECK_AND_THROW("failed to get time", err);
+
+        if (mHour.has_value() && mHour.value() != hour) {
+            return false;
+        }
+
+        if (mMinute.has_value() && mMinute.value() != minute) {
+            return false;
+        }
+
+        if (mSecond.has_value() && mSecond.value() != second) {
+            return false;
+        }
+
+        return true;
+    }
+
+private:
+    std::optional<int> mYear;
+    std::optional<int> mMonth;
+    std::optional<int> mDay;
+    std::optional<int> mHour;
+    std::optional<int> mMinute;
+    std::optional<int> mSecond;
+};
+
+} // namespace
+
+class TimeTest : public Test {
+protected:
+    void SetUp() override { test::InitLog(); }
+};
+
+/***********************************************************************************************************************
  * Tests
  **********************************************************************************************************************/
 
-namespace aos::common::utils {
-
-TEST(TimeTest, ParseDurationFromValidString)
+TEST_F(TimeTest, ParseDurationFromValidString)
 {
     struct Test {
         std::string              input;
@@ -59,13 +163,100 @@ TEST(TimeTest, ParseDurationFromValidString)
     }
 }
 
-TEST(TimeTest, ParseDurationFromInvalidString)
+TEST_F(TimeTest, ParseDurationFromInvalidString)
 {
     std::vector<std::string> tests = {"1", "1a", "1s1", "sss", "s111", "%12d", "y1y", "/12d"};
 
     for (const auto& test : tests) {
         auto [duration, error] = ParseDuration(test);
         ASSERT_FALSE(error.IsNone());
+    }
+}
+
+TEST_F(TimeTest, ParseISO8601Duration)
+{
+    using namespace std::chrono_literals;
+
+    struct Test {
+        std::string              input;
+        std::chrono::nanoseconds expected;
+    };
+
+    std::vector<Test> tests = {
+        {"P1Y", 24h * 365},
+        {"P1Y1D", 24h * 365 + 24h},
+        {"PT1S", 1s},
+        {"PT1M1S", 1min + 1s},
+        {"PT1H1M1S", 1h + 1min + 1s},
+        {"P1Y1M1W1DT1H1M1S", 24h * 365 + (24h * 365 / 12) + (24h * 7) + 24h + 1h + 1min + 1s},
+    };
+
+    for (const auto& test : tests) {
+        auto [duration, err] = ParseISO8601Duration(test.input);
+
+        ASSERT_TRUE(err.IsNone()) << "input: " << test.input << ", err: " << err.Message();
+        ASSERT_EQ(duration, test.expected) << "input: " << test.input << ", expected: " << test.expected.count() << ", "
+                                           << "actual: " << duration.count();
+    }
+}
+
+TEST_F(TimeTest, FormatISO8601Duration)
+{
+    using namespace std::chrono_literals;
+
+    struct Test {
+        std::string              expected;
+        std::chrono::nanoseconds Duration;
+    };
+
+    std::vector<Test> tests = {
+        {"PT1S", 1s},
+        {"PT1M", 1min},
+        {"PT1H", 1h},
+        {"PT1H1M1S", 1h + 1min + 1s},
+        {"P1D", 24h},
+        {"P1W", 24h * 7},
+        {"P1M", 24h * 365 / 12},
+        {"P1Y", 24h * 365},
+        {"P1Y1M1W1DT1H1M1S", 24h * 365 + (24h * 365 / 12) + (24h * 7) + 24h + 1h + 1min + 1s},
+    };
+
+    for (const auto& test : tests) {
+        auto [durationStr, error] = FormatISO8601Duration(test.Duration);
+
+        ASSERT_TRUE(error.IsNone()) << "expected: " << test.expected << ", err: " << error.Message();
+        ASSERT_EQ(durationStr, test.expected);
+    }
+}
+
+TEST_F(TimeTest, FromToUTCString)
+{
+    struct Test {
+        std::string      input;
+        std::string      expectedUTCString;
+        std::string      timezone;
+        ExpectedDateTime expectedLocalTime;
+    };
+
+    std::vector<Test> tests = {
+        {"2024-01-01T00:00:00Z", "2024-01-01T00:00:00Z", "GMT+1",
+            ExpectedDateTime().SetYear(2024).SetDay(1).SetMonth(1).SetHour(1)},
+        {"2024-01-01T00:00:00Z", "2024-01-01T00:00:00Z", "GMT-1",
+            ExpectedDateTime().SetYear(2023).SetDay(31).SetMonth(12).SetHour(23).SetMinute(0)},
+    };
+
+    for (const auto& test : tests) {
+        SetTimezone(test.timezone);
+
+        auto [time, fromError] = FromUTCString(test.input);
+        ASSERT_TRUE(fromError.IsNone());
+
+        auto [timeStr, toError] = ToUTCString(time);
+
+        ASSERT_TRUE(toError.IsNone());
+        ASSERT_EQ(timeStr, test.expectedUTCString);
+
+        ASSERT_EQ(test.expectedLocalTime, time);
     }
 }
 
